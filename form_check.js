04cf2520e54e039752e0b563fb43b17d089a8957
NadiraX <57359646+NadiraX@users.noreply.github.com>
2020-04-04 21:19:36 +0200

    ending

--- form_check.js
+++ form_check.js
@@ -1,6 +1,45 @@
 function isWhiteSpaceOrEmpty(str) {
  return /^[\s\t\r\n]*$/.test(str);
 }
+function alterRows(i, e) {
+ if (e) {
+ if (i % 2 == 1) {
+ e.setAttribute("style", "background-color: Aqua;");
+ }
+ e = e.nextSibling;
+ while (e && e.nodeType != 1) {
+ e = e.nextSibling;
+ }
+ alterRows(++i, e);
+ }
+}
+function nextNode(e) {
+ while (e && e.nodeType != 1) {
+ e = e.nextSibling;
+ }
+ return e;
+}
+function prevNode(e) {
+ while (e && e.nodeType != 1) {
+ e = e.previousSibling;
+ }
+ return e;
+}
+function cnt(form, msg, maxSize) {
+ if (form.value.length > maxSize)
+ form.value = form.value.substring(0, maxSize);
+ else
+ msg.innerHTML = maxSize - form.value.length;
+}
+
+function swapRows(b) {
+ let tab = prevNode(b.previousSibling);
+ let tBody = nextNode(tab.firstChild);
+ let lastNode = prevNode(tBody.lastChild);
+ tBody.removeChild(lastNode);
+ let firstNode = nextNode(tBody.firstChild);
+ tBody.insertBefore(lastNode, firstNode);
+}
 
 function checkStringAndFocus(obj, msg,val) {
  let str = obj.value;
@@ -16,25 +55,13 @@ function checkStringAndFocus(obj, msg,val) {
  }
 }
 
-function checkEmailAndFocus(obj,msg,val){
- let str = obj.value;
- let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
- if(isWhiteSpaceOrEmpty(str)){
- document.getElementById(errorFieldName).innerHTML = msg;
- obj.focus();
- return false;
- }
- if(!val(str)){
-	document.getElementById(errorFieldName).innerHTML = "podaj właściwy mail";
-	obj.focus();
-	return false;
- }else{
-	 document.getElementById(errorFieldName).innerHTML = null;
-	 return true;
- }
-
+function showElement(e) {
+    document.getElementById(e).style.visibility = 'visible';
 }
 
+function hideElement(e) {
+    document.getElementById(e).style.visibility = 'hidden';
+}
 
 function isEmailValid(str) {
  let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
