b6f4253fa41fccc63c6d6e6dc61a4f213f0fcd64
NadiraX <57359646+NadiraX@users.noreply.github.com>
2020-04-04 20:15:09 +0200

    21

--- form_check.js
+++ form_check.js
@@ -1,60 +1,57 @@
 function isWhiteSpaceOrEmpty(str) {
  return /^[\s\t\r\n]*$/.test(str);
 }
-function checkString(spr,msg){
-if	(isWhiteSpaceOrEmpty(spr)==true){
-	alert(msg);
-	return false;
-}
-else{
-	return true;
-}
-}
 
-function checkStringAndFocus(obj, msg) {
+function checkStringAndFocus(obj, msg,val) {
  let str = obj.value;
  let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
- if (isWhiteSpaceOrEmpty(str)) {
+ if (val(str)) {
  document.getElementById(errorFieldName).innerHTML = msg;
  obj.focus();
  return false;
  }
  else {
+ document.getElementById(errorFieldName).innerHTML = null;
  return true;
  }
 }
 
+function checkEmailAndFocus(obj,msg,val){
+ let str = obj.value;
+ let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+ if(isWhiteSpaceOrEmpty(str)){
+ document.getElementById(errorFieldName).innerHTML = msg;
+ obj.focus();
+ return false;
+ }
+ if(!val(str)){
+	document.getElementById(errorFieldName).innerHTML = "podaj właściwy mail";
+	obj.focus();
+	return false;
+ }else{
+	 document.getElementById(errorFieldName).innerHTML = null;
+	 return true;
+ }
+
+}
 
 
-function checkEmail(str) {
+function isEmailValid(str) {
  let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
  if (email.test(str))
  return true;
  else {
- alert("Podaj właściwy e-mail");
  return false;
  }
 }
 
 
-function isEmpty(p1) {
-	var n = p1.length;
-  if (n <1){
-	  return true;
-  }	 else{
-	  return false;
-  }
-}
 
 function validate(formularz) {
-	if(!checkString(formularz.elements["f_imie"].value,"podaj imie")) return false;
-	if(!checkString(formularz.elements["f_nazwisko"].value,"podaj nazwisko")) return false;
-	if(!checkString(formularz.elements["f_kod"].value,"podaj kod")) return false;
-	if(!checkString(formularz.elements["f_ulica"].value,"podaj ulica")) return false;
-	if(!checkString(formularz.elements["f_miasto"].value,"podaj miasto")) return false;
-	if(!checkString(formularz.elements["f_email"].value,"podaj email")){
-		return false;
-}else{
-	if(!checkEmail(formularz.elements["f_email"].value))return false;;
-}
+	if(!checkStringAndFocus(formularz.elements["f_imie"],"podaj imie",isWhiteSpaceOrEmpty)) return false;
+	if(!checkStringAndFocus(formularz.elements["f_nazwisko"],"podaj nazwisko",isWhiteSpaceOrEmpty)) return false;
+	if(!checkEmailAndFocus(formularz.elements["f_email"],"podaj email",isEmailValid)) return false;
+	if(!checkStringAndFocus(formularz.elements["f_kod"],"podaj kod",isWhiteSpaceOrEmpty)) return false;
+	if(!checkStringAndFocus(formularz.elements["f_ulica"],"podaj ulica",isWhiteSpaceOrEmpty)) return false;
+	if(!checkStringAndFocus(formularz.elements["f_miasto"],"podaj miasto",isWhiteSpaceOrEmpty)) return false;
 }
